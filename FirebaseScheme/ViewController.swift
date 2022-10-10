9acd0de8498dec5f56b5de961d049c012dbf2469
chithian <chithiansamony@gmail.com>
2022-10-10 09:40:05 +0700

    Initial Commit

---
+++ FirebaseScheme/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseScheme
+//
+//  Created by Samony Chithian on 10/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
