1f85479723a4bc2e3ca37360d3336ecd9b689482
chithian <chithiansamony@gmail.com>
2022-10-10 11:18:15 +0700

    First commit

--- FirebaseScheme/ViewController.swift
+++ FirebaseScheme/ViewController.swift
@@ -6,14 +6,39 @@
 //
 
 import UIKit
+import FirebaseFirestore
 
 class ViewController: UIViewController {
-
+    
+    let db = Firestore.firestore()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+    #if PROD
+        print("Prod mode")
+    #else
+        print("qa mode")
+    #endif
+        
+        
+        // Add a new document with a generated ID
+        var ref: DocumentReference? = nil
+        ref = db.collection("users").addDocument(data: [
+            "first": "Chithian",
+            "last": "Hunter",
+            "born": 2022
+        ]) { err in
+            if let err = err {
+                print("Error adding document: \(err)")
+            } else {
+                print("Document added with ID: \(ref!.documentID)")
+            }
+        }
+        
     }
-
-
+    
 }
 
